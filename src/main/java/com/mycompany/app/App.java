032c67a16e2e3763f4c364e161fc2f922f3e0507
Goutham <goutham.y@outlook.com>
2016-04-04 16:22:25 +0530

    Commit changes in branch 1

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -26,6 +26,6 @@ public class App
          */
         System.out.println("... but thankfully, Checker has our back: http://checkerframework.org");
         
-         System.out.println("Master code  updated");
+         System.out.println("Branch 1 code updated");
     }
 }
